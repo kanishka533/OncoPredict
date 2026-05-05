ae869154fb2a3a9082aea358b6371a3253a719e7
kanishka533 <kanishka.s3003@gmail.com>
2026-05-05 12:58:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -24,5 +24,8 @@ def predict_breast():
     prediction = breast_model.predict([data])
     return jsonify({"prediction": int(prediction[0])})
 
+import os
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
