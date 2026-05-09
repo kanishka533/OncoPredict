af217f386d2fe18ad751b3b57a5a56b9532616ee
kanishka533 <kanishka.s3003@gmail.com>
2026-05-09 12:23:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 import pickle
 import numpy as np
 
 app = Flask(__name__)
+CORS(app)
 
-# Load models
+# Load Models
 cervical_model = pickle.load(open("cervical_model.pkl", "rb"))
 breast_model = pickle.load(open("breast_model.pkl", "rb"))
 
@@ -12,20 +14,29 @@ breast_model = pickle.load(open("breast_model.pkl", "rb"))
 def home():
     return "OncoPredict API Running"
 
+# Cervical Cancer Prediction
 @app.route("/predict_cervical", methods=["POST"])
 def predict_cervical():
-    data = request.json["features"]
-    prediction = cervical_model.predict([data])
-    return jsonify({"prediction": int(prediction[0])})
 
+    data = request.json["features"]
+
+    prediction = cervical_model.predict([data])
+
+    return jsonify({
+        "prediction": int(prediction[0])
+    })
+
+# Breast Cancer Prediction
 @app.route("/predict_breast", methods=["POST"])
 def predict_breast():
-    data = request.json["features"]
-    prediction = breast_model.predict([data])
-    return jsonify({"prediction": int(prediction[0])})
 
-import os
+    data = request.json["features"]
+
+    prediction = breast_model.predict([data])
+
+    return jsonify({
+        "prediction": int(prediction[0])
+    })
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+    app.run(host="0.0.0.0", port=10000)
