408fb1c4226d3e4d84b72ed1b1c3117ce0f93888
kanishka533 <kanishka.s3003@gmail.com>
2026-05-11 08:53:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -15,36 +15,49 @@ breast_model = pickle.load(open("breast_model.pkl", "rb"))
 def home():
     return "OncoPredict API Running"
 
-# Cervical Prediction
 @app.route("/predict_cervical", methods=["POST"])
 def predict_cervical():
 
-    data = request.json["features"]
+    try:
+        data = request.json["features"]
 
-    # Convert into numpy array
-    features = np.array(data).reshape(1, -1)
+        features = np.array(data).reshape(1, -1)
 
-    prediction = cervical_model.predict(features)
+        prediction = cervical_model.predict(features)
 
-    return jsonify({
-        "prediction": int(prediction[0])
-    })
+        return jsonify({
+            "prediction": int(prediction[0])
+        })
 
-# Breast Prediction
+    except Exception as e:
+        return jsonify({
+            "error": str(e)
+        })
+
 @app.route("/predict_breast", methods=["POST"])
 def predict_breast():
 
-    data = request.json["features"]
+    try:
+        data = request.json["features"]
 
-    # Convert into numpy array
-    features = np.array(data).reshape(1, -1)
+        features = np.array(data).reshape(1, -1)
 
-    prediction = breast_model.predict(features)
+        prediction = breast_model.predict(features)
 
-    return jsonify({
-        "prediction": int(prediction[0])
-    })
+        return jsonify({
+            "prediction": int(prediction[0])
+        })
+
+    except Exception as e:
+        return jsonify({
+            "error": str(e)
+        })
 
 if __name__ == "__main__":
+
     port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)vv
+
+    app.run(
+        host="0.0.0.0",
+        port=port
+    )
